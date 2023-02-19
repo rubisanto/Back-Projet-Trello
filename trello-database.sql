6dbb81554673b83fa12ddc47f1caf10fc5ce3831
Thomas Kieffer <thmskffr@gmail.com>
2023-02-19 01:05:39 +0100

    edited sql to fix case in status

--- trello-database.sql
+++ trello-database.sql
@@ -1,12 +1,27 @@
 -- création de la table
 CREATE TABLE IF NOT EXISTS tasks (
-	id SERIAL PRIMARY KEY,
-	title VARCHAR(50) NOT NULL,
-	category VARCHAR(11) NOT NULL
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    category VARCHAR(11) NOT NULL
 );
 
 -- Pour tester 
-INSERT INTO tasks (title, category) VALUES ('test 1', 'IN_PROGRESS');
-INSERT INTO tasks (title, category) VALUES ('test 2', 'TODO');
-INSERT INTO tasks (title, category) VALUES ('test 3', 'DONE');
-INSERT INTO tasks (title, category) VALUES ('test 4', 'BLOCKED');
+INSERT INTO
+    tasks (title, category)
+VALUES
+    ('test 1', 'In Progress');
+
+INSERT INTO
+    tasks (title, category)
+VALUES
+    ('test 2', 'To Do');
+
+INSERT INTO
+    tasks (title, category)
+VALUES
+    ('test 3', 'Done');
+
+INSERT INTO
+    tasks (title, category)
+VALUES
+    ('test 4', 'Blocked');
