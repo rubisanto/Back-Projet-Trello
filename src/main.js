7d23c3294729ff4b24c697e1db0ddc7ebd22424b
Antoine Gogce <antoinegogceapb@hotmail.fr>
2023-02-20 16:01:30 +0100

    railway env

--- src/main.js
+++ src/main.js
@@ -14,15 +14,15 @@ app.use((err, req, res, next) => {
   res.status(500).send({ error: err.message, status: 500 })
 })
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 function start() {
   Pool.connect({
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
   })
     .then(() => {
       console.log('Connexion à Postgres établie avec succès.')
